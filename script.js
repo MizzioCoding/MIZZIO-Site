09e8bf42c359d284a84bde738dd690edf547d417
Agustin Chizzini Melo <57108480+chizzi01@users.noreply.github.com>
2022-05-24 12:51:25 -0300

    nav fix

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    
-    document.getElementsByClassName("navbar").classList.add("solid");
-  } else {
-   
-    document.getElementsByClassName("navbar").classList.remove("solid");
-  }
-}
+let navbarBack = document.querySelector(".navbar");
 
+function observer(targetId, classAdd) {
+
+    const texto = document.getElementById(targetId);
+    const callback = (entradas, opciones) =>{
+    
+    entradas.forEach((entrada) => {
+        if(entrada.isIntersecting){
+            navbarBack.classList.toggle(classAdd);
+        }
+        else{
+            navbarBack.classList.remove(classAdd);
+        }
+        
+    });
+    }
+    const opciones = new IntersectionObserver(callback, {
+        root:null,
+        rootMargin:"0px 0px 0px 0px",
+        threshold:0.2
+    
+    });
+    opciones.observe(texto);
+    }
+    
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
@@ -24,3 +39,6 @@ navbarLinks.addEventListener('click', () => {
     navbarLinks.classList.remove('active');
     navbar.classList.remove('navbarHeight');
 });
+
+
+observer("nosotros", 'solid');
