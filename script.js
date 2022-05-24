f06f2c9df3b6b5dc32195aa4f23f42626386ebb7
Agustin Chizzini Melo <57108480+chizzi01@users.noreply.github.com>
2022-05-24 11:33:16 -0300

    nosotros update

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-    // Transition effect for navbar 
-    $(window).scroll(function() {
-      // checks if window is scrolled more than 500px, adds/removes solid class
-      if($(this).scrollTop() > 500) { 
-          $('.navbar').addClass('solid');
-      } else {
-          $('.navbar').removeClass('solid');
-      }
-    });
-});
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    
+    document.getElementsByClassName("navbar").classList.add("solid");
+  } else {
+   
+    document.getElementsByClassName("navbar").classList.remove("solid");
+  }
+}
 
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
