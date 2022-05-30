a3b0930ffe233df7ce5f40a46070ced4083132a8
Agustin Chizzini Melo <57108480+chizzi01@users.noreply.github.com>
2022-05-30 15:33:55 -0300

    navbar scroll fix

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 let navbarBack = document.querySelector(".navbar");
 
-function observer(targetId, classAdd) {
+// function observer(targetId, classAdd) {
 
-    const texto = document.getElementById(targetId);
-    const callback = (entradas, opciones) =>{
-    
-    entradas.forEach((entrada) => {
-        if(entrada.isIntersecting){
-            navbarBack.classList.add(classAdd);
-            navbarBack.classList.remove("unsolid");
-        }
-        else{
-            if(document.querySelector(".navbar").classList.contains(classAdd)){
-                navbarBack.classList.add("unsolid");
-            }
-            navbarBack.classList.remove(classAdd);
-        }
-        
-    });
+//     const texto = document.getElementById(targetId);
+//     const callback = (entradas, opciones) =>{
+
+//     entradas.forEach((entrada) => {
+//         if(entrada.isIntersecting){
+//             navbarBack.classList.add(classAdd);
+//             navbarBack.classList.remove("unsolid");
+//         }
+//         else{
+//             if(document.querySelector(".navbar").classList.contains(classAdd)){
+//                 navbarBack.classList.add("unsolid");
+//             }
+//             navbarBack.classList.remove(classAdd);
+//         }
+
+//     });
+//     }
+//     const opciones = new IntersectionObserver(callback, {
+//         root:null,
+//         rootMargin:"0px 0px 0px 0px",
+//         threshold:0.2
+
+//     });
+//     opciones.observe(texto);
+//     }
+// observer("nosotros", 'solid');
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 50) {
+        $('.navbar').addClass('solid');
+        $('.navbar').removeClass('unsolid');
+    } else {
+        $('.navbar').removeClass('solid');
+        $('.navbar').addClass('unsolid');
+
     }
-    const opciones = new IntersectionObserver(callback, {
-        root:null,
-        rootMargin:"0px 0px 0px 0px",
-        threshold:0.2
-    
-    });
-    opciones.observe(texto);
-    }
-    
+});
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
@@ -36,7 +48,7 @@ const navbar = document.getElementsByClassName('navbar')[0];
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
     navbar.classList.toggle('navbarHeight');
-    
+
 });
 
 navbarLinks.addEventListener('click', () => {
@@ -45,27 +57,26 @@ navbarLinks.addEventListener('click', () => {
 });
 
 
-observer("nosotros", 'solid');
 
-$(document).ready(function(){
+$(document).ready(function () {
     let heightDiv = $(".b").height();
     $('.rect-container').height(heightDiv);
     console.log(heightDiv);
 });
 
-$("#paqueteOne").click(function (e) { 
+$("#paqueteOne").click(function (e) {
     e.preventDefault();
     $("#paquete").val("PAQUETE ONE PAGE");
     window.location.replace("#contacto");
 });
 
-$("#paqueteAvanzado").click(function (e) { 
+$("#paqueteAvanzado").click(function (e) {
     e.preventDefault();
     $("#paquete").val("PAQUETE AVANZADO");
     window.location.replace("#contacto");
 });
 
-$("#paqueteEcomerce").click(function (e) { 
+$("#paqueteEcomerce").click(function (e) {
     e.preventDefault();
     $("#paquete").val("PAQUETE E-COMMERCE");
     window.location.replace("#contacto");
@@ -74,26 +85,26 @@ $("#paqueteEcomerce").click(function (e) {
 
 let textChange = document.querySelector(".textChange");
 
-setTimeout(function(){
+setTimeout(function () {
     textChange.innerHTML = "Diseñá";
 }, 2000);
 
-setTimeout(function(){
+setTimeout(function () {
     textChange.innerHTML = "";
 }, 2500);
 
-setTimeout(function(){
+setTimeout(function () {
     textChange.innerHTML = "Renová";
 }, 3000);
 
-setTimeout(function(){
+setTimeout(function () {
     textChange.innerHTML = "";
 }, 3500);
 
-setTimeout(function(){
+setTimeout(function () {
     textChange.innerHTML = "Mejorá";
 }, 4000);
 
-setTimeout(function(){
+setTimeout(function () {
     textChange.innerHTML = " Mejorá tu página web";
 }, 4500);
