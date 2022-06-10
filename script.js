e9af50bb6d0088819e68ab79f0604667ba92b211
Agustin Chizzini Melo <57108480+chizzi01@users.noreply.github.com>
2022-06-10 09:08:46 -0300

    wpp icon

--- script.js
+++ script.js
@@ -115,3 +115,26 @@ setTimeout(function () {
 setTimeout(function () {
     textChange.innerHTML = "Creá tu página web";
 }, 5000);
+
+
+
+
+let mybutton = document.getElementById("wppIcon");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+$("#wppIcon").hide();
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
