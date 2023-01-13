fe2f806facfaa2f62220249b899af4011bd86180
Agustin Chizzini Melo <aguscn2001@gmail.com>
2023-01-13 00:22:01 -0300

    Form btn update

--- script.js
+++ script.js
@@ -147,61 +147,61 @@ reloadCache.addEventListener("click", function () {
 
 var basicTimeline = anime.timeline({
     autoplay: false
-  });
-  
-  var pathEls = $(".check");
-  for (var i = 0; i < pathEls.length; i++) {
+});
+
+var pathEls = $(".check");
+for (var i = 0; i < pathEls.length; i++) {
     var pathEl = pathEls[i];
     var offset = anime.setDashoffset(pathEl);
     pathEl.setAttribute("stroke-dashoffset", offset);
-  }
-  
-  basicTimeline
+}
+
+basicTimeline
     .add({
-      targets: ".text",
-      duration: 1,
-      opacity: "0"
+        targets: ".text",
+        duration: 1,
+        opacity: "0"
     })
     .add({
-      targets: ".button",
-      duration: 1300,
-      height: 10,
-      width: 300,
-      backgroundColor: "#2B2D2F",
-      border: "0",
-      borderRadius: 100
+        targets: ".button",
+        duration: 1300,
+        height: 10,
+        width: 300,
+        backgroundColor: "#2B2D2F",
+        border: "0",
+        borderRadius: 100
     })
     .add({
-      targets: ".progress-bar",
-      duration: 2000,
-      width: 300,
-      easing: "linear"
+        targets: ".progress-bar",
+        duration: 2000,
+        width: 300,
+        easing: "linear"
     })
     .add({
-      targets: ".button",
-      width: 0,
-      duration: 1
+        targets: ".button",
+        width: 0,
+        duration: 1
     })
     .add({
-      targets: ".progress-bar",
-      width: 80,
-      height: 80,
-      delay: 500,
-      duration: 750,
-      borderRadius: 80,
-      backgroundColor: "#71DFBE"
+        targets: ".progress-bar",
+        width: 80,
+        height: 80,
+        delay: 500,
+        duration: 750,
+        borderRadius: 80,
+        backgroundColor: "#71DFBE"
     })
     .add({
-      targets: pathEl,
-      strokeDashoffset: [offset, 0],
-      duration: 200,
-      easing: "easeInOutSine"
+        targets: pathEl,
+        strokeDashoffset: [offset, 0],
+        duration: 200,
+        easing: "easeInOutSine"
     });
-  
-  $(".button").click(function() {
+
+$(".button").click(function () {
     basicTimeline.play();
-  });
-  
-  $(".text").click(function() {
+});
+
+$(".text").click(function () {
     basicTimeline.play();
-  });
+});
